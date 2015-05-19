dc1023c0d2cbcaea7570379cf356947b6fd96814
lc4t <lc4t0.0@gmail.com>
2015-05-19 17:14:09 +0800

    BGM BGI done

--- main.cpp
+++ main.cpp
@@ -1,10 +1,51 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include "Includes/Config.hpp"
+
+
+using namespace std;
 using namespace sf;
+
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(400, 600), "Fighters");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	//window load
+	sf::RenderWindow window(sf::VideoMode(400, 600), "Fighters");
+
+	// BGI load init
+	sf::Texture background;
+	if (!background.loadFromFile(bgiPath))
+		{
+			cout<<"Error to load background"<<endl;
+		}
+	sf::Sprite bg(background);
+	// BGI load init end
+
+	//BGM load
+	sf::Music music;
+	if (!music.openFromFile(bgmPath))
+	    return EXIT_FAILURE;
+	music.play();
+	//BGM load end
+
+
+
+    //font load
+    sf::Font font;
+    if (!font.loadFromFile("Resources/font/STHeiti Light.ttc"))
+		{
+			cout<<"Error load font"<<endl;
+		}
+    sf::Text text;
+    text.setFont(font);
+    text.setString("Hello world");
+    text.setCharacterSize(24);
+    text.setColor(sf::Color::Red);
+    text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+
+
+
 
     while (window.isOpen())
     {
@@ -16,7 +57,8 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(bg);		//BGI
+        window.draw(text);		//font
         window.display();
     }
 
